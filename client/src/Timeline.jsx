783220590fd271a2f7ad58f08a997547fc1d59d4
Phoebe <meipbe@gmail.com>
2017-11-28 17:08:55 -0800

    Make createEventBox available to individual days

--- client/src/Timeline.jsx
+++ client/src/Timeline.jsx
@@ -3,19 +3,36 @@ import _ from 'lodash';
 import propTypes from 'prop-types';
 import DayView from './Day';
 
-const Timeline = ({ timelineData, timelineId }) => (
-  <div className="container timeline">
-    <div className="container day">
-      {_.map(timelineData, (day, index) => (
-        <DayView
-          timelineId={timelineId}
-          day={day}
-          key={index}
-        />
-      ))}
+const Timeline = (props) => {
+  const {
+    timelineData,
+    timelineId,
+    timelineName,
+    onCreateEnter,
+    handleNewEvent,
+    handleNewAddress,
+    createEvent,
+  } = props;
+
+  return (
+    <div className="container timeline">
+      <div>{timelineName}</div>
+      <div className="container day">
+        {_.map(timelineData, (day, index) => (
+          <DayView
+            timelineId={timelineId}
+            day={day}
+            key={index}
+            onCreateEnter={onCreateEnter}
+            handleNewEvent={handleNewEvent}
+            handleNewAddress={handleNewAddress}
+            createEvent={createEvent}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Timeline.propTypes = {
   timelineData: propTypes.instanceOf(Array).isRequired,
