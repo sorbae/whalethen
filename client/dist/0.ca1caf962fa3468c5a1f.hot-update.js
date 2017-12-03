8485d46ee9a8872e4f559ceda169e579cf19791c
Phoebe <meipbe@gmail.com>
2017-12-02 19:13:07 -0800

    Heroku

---
+++ client/dist/0.ca1caf962fa3468c5a1f.hot-update.js
@@ -0,0 +1,27 @@
+webpackHotUpdate(0,{
+
+/***/ 417:
+/*!*******************************!*\
+  !*** ./client/src/Events.jsx ***!
+  \*******************************/
+/*! dynamic exports provided */
+/*! all exports used */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed: SyntaxError: Adjacent JSX elements must be wrapped in an enclosing tag (116:6)\\n\\n\\u001b[0m \\u001b[90m 114 | \\u001b[39m          {\\u001b[36mthis\\u001b[39m\\u001b[33m.\\u001b[39mstate\\u001b[33m.\\u001b[39mcommentView \\u001b[33m&&\\u001b[39m commentBox}\\n \\u001b[90m 115 | \\u001b[39m        \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mdiv\\u001b[39m\\u001b[33m>\\u001b[39m\\n\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 116 | \\u001b[39m      \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mdiv\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[33m,\\u001b[39m\\n \\u001b[90m     | \\u001b[39m      \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\n \\u001b[90m 117 | \\u001b[39m    )\\u001b[33m;\\u001b[39m\\n \\u001b[90m 118 | \\u001b[39m  }\\n \\u001b[90m 119 | \\u001b[39m}\\u001b[0m\\n\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNDE3LmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///417\n");
+
+/***/ }),
+
+/***/ 418:
+false,
+
+/***/ 419:
+false,
+
+/***/ 420:
+false,
+
+/***/ 421:
+false
+
+})
