1d9fc4420506e5460e982df4731b9d6afd0ac3b1
Phoebe <meipbe@gmail.com>
2017-12-02 17:20:21 -0800

    production

---
+++ webpack.prod.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const DIST_DIR = path.resolve(__dirname, 'client/dist');
+const SRC_DIR = path.resolve(__dirname, 'client/');
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: [
+    `${SRC_DIR}/index.js`,
+  ],
+
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+    publicPath: '/client/',
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: 'production',
+      },
+    }),
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        },
+      },
+    ],
+  },
+};
