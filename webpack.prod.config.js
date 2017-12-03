190db443f93fc8ed6fb428ef9882722c73b75261
Phoebe <meipbe@gmail.com>
2017-12-03 14:10:56 -0800

    Heroku

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -20,7 +20,8 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: 'production',
+        NODE_ENV: JSON.stringify('production'),
+        API_HOST: 'https://whalezthen.herokuapp.com/',
       },
     }),
   ],
