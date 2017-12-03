35230301146417b6fbd7799f62c8bbb3f5a28ff4
Phoebe <meipbe@gmail.com>
2017-12-02 19:07:07 -0800

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,12 @@ const config = {
   module: {
     loaders: [
       {
-        test: /\.jsx?/,
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
         include: SRC_DIR,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'env'],
+          presets: ['react', 'es2015', 'env'],
         },
       },
     ],
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -28,11 +28,12 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.jsx?/,
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
         include: SRC_DIR,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'env'],
+          presets: ['react', 'es2015', 'env'],
         },
       },
     ],
